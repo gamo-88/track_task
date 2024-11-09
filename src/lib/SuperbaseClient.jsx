7b0f55726cf62ac6146c8173b7773b9811b5be6e
gamo-88 <maximegamo@gmail.com>
2024-11-09 17:26:33 +0100

    removeKey

--- src/lib/SuperbaseClient.jsx
+++ src/lib/SuperbaseClient.jsx
@@ -1,10 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vnghlakudkgjasouevnt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuZ2hsYWt1ZGtnamFzb3Vldm50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMyMjUzNDIsImV4cCI6MjAzODgwMTM0Mn0.PTZllC23x-ij9te4JaQ1H4ZjcFczX16RLPI8iLi10aI'
-// const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
-// ess.env.SUPABASE_KEY
